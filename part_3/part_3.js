a40ff0e688a0e3d2d101b231eb0f2ba6490afc8b
unknown <omar.q.alkhatib@gmail.comgit config --global user.name Omar-Alkhatib git config --global user.email "omar.q.alkhatib@gmail.comgit config --global user.name "Omar-Alkhatib>
2021-01-01 21:47:29 +0200

    part_3 solved

--- part_3/part_3.js
+++ part_3/part_3.js
@@ -6,31 +6,47 @@ Type the correct answer below each question:
 
 Q1) Why do we use joins in SQL?
 
+To obtain information accross different tables based on a mutual key.
+
 
 Q2) Name three types of relationships in databases.
 
+one to one 
+one to many 
+many to many
 
 Q3) What are the main three sections of a JWT?
 
+header
+payload 
+options
 
 Q4) Name three types of express middle-wares.
 
+App_level middleware 
+component_level 
 
 Q5) What is the `effect` hook used for in react?
+Coponent life cycle. 
 
 
 Q6) What is the `state` hook used for in react?
+To save and change the state of a component. 
 
 
 Q7) List three thing you can use to create a responsive web application.
-
+Grid layout
+list layout
+list box
 
 Q8) Name two of mongoose middle-wares and explain what they are used for
 
 
 Q9) What is the difference between authentication and authorization?
 
+authentication is to approve the user to use the platform. 
+Authorization is to allow an authenticated user to perform a specific operation. 
 
 Q10) Why do we save some information as an environment variable (in .env file)?
-
+For security reasons. To be kept safe and separated from the code. 
 */
